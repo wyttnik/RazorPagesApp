c8044dab9f10e761e840e59a8e262489eeddec91
wyttnik <thevladiinoy@gmail.com>
2023-03-19 03:06:28 +0300

    First commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Testimonials.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesApp.Pages
+{
+    public class TestimonialsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
